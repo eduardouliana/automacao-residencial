fd90941b2c4d18afe291193cb83d29f8645034a8
Eduardo Uliana <edu02_rko@hotmail.com>
2019-07-26 00:02:16 -0300

    Adicionado arquivo de comandos de voz para teste

--- codigoNodeMcu/codigoNodeMcu.ino
+++ codigoNodeMcu/codigoNodeMcu.ino
@@ -6,17 +6,22 @@
 #include <ArduinoJson.h>
 
 // URL do Firebase SEM o "HTTPS" e SEM A BARRA "/" no final
-#define FIREBASE_HOST "xxxxxxxxxxx.firebaseio.com"
+#define FIREBASE_HOST "*********.firebaseio.com"
 // Key do Firebase
-#define FIREBASE_AUTH "hLo5v06**************rdQNYRUvop"
+#define FIREBASE_AUTH "hLo5v06E**************rdQNYRUvop"
 // Nome da rede Wifi
 #define WIFI_SSID "CASA_GELADA"
 // Senha do Wifi
-#define WIFI_PASSWORD "#passarinhos"
+#define WIFI_PASSWORD "*************"
 
 // Define em qual pino está conectado
 int pinoPorta = D6;
 int pinoLuzes = D7;
+
+// Pinos para led RGB
+int pinoRGBAzul = D1;
+int pinoRGBVerde = D2;
+int pinoRGBVermelho = D3;
 
 // Variáveis para ler do Firebase
 String statusPorta = "";
@@ -24,13 +29,19 @@ String statusLuzes = "";
 String statusPortaANT = "";
 String statusLuzesANT = "";
 
+String corRGB = "";
+
 void setup() {
   Serial.begin(9600);
   delay(1000);
   
   // Define os pinos como saída
   pinMode(pinoPorta, OUTPUT);
-  pinMode(pinoLuzes, OUTPUT);                 
+  pinMode(pinoLuzes, OUTPUT); 
+  // RGB
+  pinMode(pinoRGBAzul, OUTPUT);
+  pinMode(pinoRGBVerde, OUTPUT);
+  pinMode(pinoRGBVermelho, OUTPUT);                
 
   // Tenta conectar com Wifi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -59,24 +70,100 @@ void loop() {
   statusPorta = Firebase.getString("STATUS_PORTA");
   statusLuzes = Firebase.getString("STATUS_LUZES");
 
+  corRGB = Firebase.getString("COR_RGB");
+
   // Verifica se é para abrir a porta
   if (statusPorta != statusPortaANT) {  
     if (statusPorta == "ABRIR") {
-      Serial.println("Abrindo porta...");
-      digitalWrite(pinoPorta, HIGH);
+      abrirPorta();
     } else {
-      Serial.println("Fechando porta...");
-      digitalWrite(pinoPorta, LOW);
+      fecharPorta();
     }
   }
   // Verifica se é para ligar as luzes
   if (statusLuzes != statusLuzesANT) {  
     if (statusLuzes == "LIGAR") {
-      Serial.println("Ligando luzes...");
-      digitalWrite(pinoLuzes, HIGH);
+      ligarLuzes();
     } else {
-      Serial.println("Desligando luzes...");
-      digitalWrite(pinoLuzes, LOW);
+      desligarLuzes();
     }
   }
+
+  if (corRGB == "VERMELHO") {
+    ligarRGBVermelho();
+  } else if (corRGB == "AZUL") {
+    ligarRGBAzul();
+  } else if (corRGB == "VERDE") {
+    ligarRGBVerde();
+  } else if (corRGB == "AMARELO") {
+    ligarRGBAmarelo();
+  } else if (corRGB == "ROXO") {
+    ligarRGBRoxo();
+  } else if (corRGB == "BRANCO") {
+    ligarRGBBranco();
+  } else if (corRGB == "DESLIGAR") {
+    desligarRGB();
+  }
+}
+
+void abrirPorta() {
+  Serial.println("Abrindo porta...");
+  digitalWrite(pinoPorta, HIGH);
+}
+
+void fecharPorta() {
+  Serial.println("Fechando porta...");
+  digitalWrite(pinoPorta, LOW);
+}
+
+void ligarLuzes() {
+  Serial.println("Ligando luzes...");
+  digitalWrite(pinoLuzes, HIGH);
+}
+
+void desligarLuzes() {
+  Serial.println("Desligando luzes...");
+  digitalWrite(pinoLuzes, LOW);
+}
+
+void ligarRGBVermelho(){
+  digitalWrite(pinoRGBAzul, LOW);
+  digitalWrite(pinoRGBVerde, LOW);
+  digitalWrite(pinoRGBVermelho, HIGH);
+}
+
+void ligarRGBAzul(){
+  digitalWrite(pinoRGBAzul, HIGH);
+  digitalWrite(pinoRGBVerde, LOW);
+  digitalWrite(pinoRGBVermelho, LOW);
+}
+
+void ligarRGBVerde(){
+  digitalWrite(pinoRGBAzul, LOW);
+  digitalWrite(pinoRGBVerde, HIGH);
+  digitalWrite(pinoRGBVermelho, LOW);
+}
+
+void ligarRGBAmarelo(){
+  analogWrite(pinoRGBAzul, 0);
+  analogWrite(pinoRGBVerde, 50);
+  analogWrite(pinoRGBVermelho, 255);
+}
+
+void ligarRGBRoxo(){
+  analogWrite(pinoRGBAzul, 207);
+  analogWrite(pinoRGBVerde, 0);
+  analogWrite(pinoRGBVermelho, 255);
+}
+
+void ligarRGBBranco(){
+  digitalWrite(pinoRGBAzul, HIGH);
+  digitalWrite(pinoRGBVerde, HIGH);
+  digitalWrite(pinoRGBVermelho, HIGH);
+}
+
+void desligarRGB(){
+  digitalWrite(pinoRGBAzul, LOW);
+  digitalWrite(pinoRGBVerde, LOW);
+  digitalWrite(pinoRGBVermelho, LOW);
 }
